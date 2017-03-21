e78d2e321c1e63b642434107a01e284909db8e19
Boris Demeshev <boris.demeshev@gmail.com>
2017-03-21 21:03:01 +0300

    kr 03 test solution

--- 2016/stan_models/monetki.R
+++ 2016/stan_models/monetki.R
@@ -6,10 +6,19 @@ model <- stan_model(file = "monetki.stan")
 
 monetki_data <- list(N = 2, y = c(1, 1))
 
-fit <- sampling(model, data = monetki_data)
+fit <- sampling(model, data = monetki_data, seed = 42)
+
+
+
+?vb
 fit
 
 fit_array <- as.array(fit)
 fit_array
 
 mcmc_hist(fit_array)
+
+# approximate ADVI 
+fit_advi <- vb(model, data = monetki_data, seed = 43, iter = 10000)
+fit_advi
+
