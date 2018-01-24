1e1dbf4d7dd6567a799b26668af93ef8c1c53804
Boris Demeshev <boris.demeshev@gmail.com>
2018-01-24 19:07:56 +0300

    update sem 2

--- 2017/sem_r_for_finance/sem_r_for_finance_2.R
+++ 2017/sem_r_for_finance/sem_r_for_finance_2.R
@@ -13,4 +13,26 @@ file.choose()
 # look at the data
 glimpse(flats)
 
+# some plots
+qplot(data = flats, x = price)
 
+# maybe I should take logs?
+qplot(data = flats, x = log(price))
+
+# scatter plots
+qplot(data = flats, y = log(price), x = log(totsp))
+
+# basic operations with data frame
+
+# 1. create new variable
+flats2 <- mutate(flats, ln_price = log(price))
+glimpse(flats2)
+
+# result in flats3
+# othersp = totsp - kitsp - livesp
+flats3 <- mutate(flats2, othersp = totsp - livesp - kitsp)
+
+# 2. filtering observations
+flats4 <- filter(flats3, price > mean(price))
+
+# filter flats in zone 1 of moscow (center), look at the code variable
